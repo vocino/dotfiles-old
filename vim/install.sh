eb52c6e4ef89ce4e2b8bbc477e60f75262b17705
Travis Vocino <travis@vocino.com>
2014-08-01 19:32:07 -0700

    Add vimrc and custom npm registry

--- vim/install.sh
+++ vim/install.sh
@@ -25,6 +25,8 @@ get_bundle pangloss vim-javascript
 get_bundle rking ag.vim
 get_bundle therubymug vim-pyte
 get_bundle tomasr molokai
+
+# tpope stuff
 get_bundle tpope vim-abolish
 get_bundle tpope vim-bundler
 get_bundle tpope vim-commentary
@@ -48,8 +50,17 @@ get_bundle tpope vim-surround
 get_bundle tpope vim-tbone
 get_bundle tpope vim-unimpaired
 get_bundle tpope vim-vividchalk
+
+# scripts
 get_bundle vim-scripts bufexplorer.zip
 get_bundle vim-scripts bufkill.vim
+
+# navigation
+get_bundle shougo unite.vim
+get_bundle scrooloose nerdtree
+
+# vanity
+get_bundle bling vim-airline
 get_bundle zenorocha dracula-theme
 
 vim -c 'call pathogen#helptags()|q'
